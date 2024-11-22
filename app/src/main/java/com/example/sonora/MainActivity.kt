14635c64b3eb3a7c280be5b50e36efc6300f1177
makzs <erikmakiak@gmail.com>
2024-11-22 10:43:08 -0300

    implementando seções na pagina principal

--- app/src/main/java/com/example/sonora/MainActivity.kt
+++ app/src/main/java/com/example/sonora/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.sonora
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sonora.Adapter.CategoryAdapter
+import com.example.sonora.Adapter.SectionSongListAdapter
 import com.example.sonora.Models.CategoryModel
 import com.example.sonora.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
@@ -21,9 +27,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getCategories()
+        setupSection("secion_1", binding.section1MainLayout, binding.section1Title, binding.section1RecyclerView)
+        setupSection("secion_2", binding.section2MainLayout, binding.section2Title, binding.section2RecyclerView)
 
     }
 
+    // categorias
     fun getCategories() {
         FirebaseFirestore.getInstance().collection("Category").get().addOnSuccessListener {
             val categoryList = it.toObjects(CategoryModel::class.java)
@@ -36,5 +45,25 @@ class MainActivity : AppCompatActivity() {
         binding.categoriesRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         binding.categoriesRecyclerView.adapter = categoryAdapter
     }
+
+    // Sections
+    fun setupSection(id : String, mainLayout : RelativeLayout, titleView: TextView, recyclerView: RecyclerView){
+        FirebaseFirestore.getInstance().collection("sections")
+            .document(id)
+            .get().addOnSuccessListener {
+                val section = it.toObject(CategoryModel::class.java)
+                section?.apply {
+                    mainLayout.visibility = View.VISIBLE
+                    titleView.text = name
+                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+                    recyclerView.adapter = SectionSongListAdapter(songs)
+                    mainLayout.setOnClickListener{
+                        SongsListActivity.category = section
+                        startActivity(Intent(this@MainActivity, SongsListActivity::class.java))
+                    }
+                }
+            }
+    }
+
 
 }
